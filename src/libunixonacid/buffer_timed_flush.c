255d410c6492e42a584880785d63560dc96acdb4
Laurent Bercot <ska-skaware@skarnet.org>
2014-12-15 23:23:37 +0000

    Fix: timed_buffer_flush -> buffer_timed_flush

--- src/libunixonacid/buffer_timed_flush.c
+++ src/libunixonacid/buffer_timed_flush.c
@@ -10,7 +10,7 @@ static int buffer_isnonempty (buffer *b)
   return !buffer_isempty(b) ;
 }
 
-int timed_buffer_flush (buffer *b, tain_t const *deadline, tain_t *stamp)
+int buffer_timed_flush (buffer *b, tain_t const *deadline, tain_t *stamp)
 {
   return timed_flush(b, (initfunc_t_ref)&buffer_getfd, (initfunc_t_ref)&buffer_isnonempty, (initfunc_t_ref)&buffer_flush, deadline, stamp) ;
 }
