6c96f05a25f1564b5e12366544677287fc201ef9
Laurent Bercot <ska-skaware@skarnet.org>
2015-07-26 22:24:08 +0000

    Bugfix: wrong storage size in socket_local46 (thanks Jesse Young)

--- src/libstddjb/socket_local46.c
+++ src/libstddjb/socket_local46.c
@@ -12,17 +12,17 @@
 
 int socket_local46 (int s, ip46_t *ip, uint16 *port)
 {
-  struct sockaddr sa ;
+  struct sockaddr_storage sa ;
   socklen_t dummy = sizeof sa ;
-  if (getsockname(s, &sa, &dummy) < 0) return -1 ;
-  if (sa.sa_family == AF_INET6)
+  if (getsockname(s, (struct sockaddr *)&sa, &dummy) < 0) return -1 ;
+  if (sa.ss_family == AF_INET6)
   {
     register struct sockaddr_in6 *sa6 = (struct sockaddr_in6 *)&sa ;
     byte_copy(ip->ip, 16, (char const *)sa6->sin6_addr.s6_addr) ;
     uint16_unpack_big((char *)&sa6->sin6_port, port) ;
     ip->is6 = 1 ;
   }
-  else if (sa.sa_family == AF_INET)
+  else if (sa.ss_family == AF_INET)
   {
     register struct sockaddr_in *sa4 = (struct sockaddr_in *)&sa ;
     byte_copy(ip->ip, 4, (char const *)&sa4->sin_addr.s_addr) ;
