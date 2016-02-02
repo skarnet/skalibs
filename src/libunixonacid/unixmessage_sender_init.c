6129e14bc76488f121c4557eb477886dd9dd2926
Laurent Bercot <ska-skaware@skarnet.org>
2016-02-02 07:36:22 +0000

    Revert " Add support for advanced closing callbacks to unixmessage_sender_flush."
    This reverts commit c9c7e241a7b68f67e0cc8a7d3874510dced532d6.
    
    (something is segfaulting, reverting until I find the fix)

--- src/libunixonacid/unixmessage_sender_init.c
+++ src/libunixonacid/unixmessage_sender_init.c
@@ -1,8 +1,15 @@
 /* ISC license. */
 
+#include <skalibs/stralloc.h>
+#include <skalibs/genalloc.h>
 #include <skalibs/unixmessage.h>
 
 void unixmessage_sender_init (unixmessage_sender_t *b, int fd)
 {
-  unixmessage_sender_init_withclosecb(b, fd, &unixmessage_sender_closecb, 0) ;
+  b->fd = fd ;
+  b->data = stralloc_zero ;
+  b->fds = genalloc_zero ;
+  b->offsets = genalloc_zero ;
+  b->head = 0 ;
+  b->shorty = 0 ;
 }
