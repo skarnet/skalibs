00e0cad87182eebc8af16172c1dedb10761ffded
Laurent Bercot <ska-skaware@skarnet.org>
2016-04-27 17:32:40 +0000

    Fix tryfutimens.c header inclusion. The test should be good now...

--- src/sysdeps/tryfutimens.c
+++ src/sysdeps/tryfutimens.c
@@ -6,6 +6,10 @@
 #define __EXTENSIONS__
 #endif
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+
 int main (void)
 {
   struct timespec foo[2] = { { .tv_sec = 0, .tv_nsec = 0 }, { .tv_sec = 0, .tv_nsec = 0 } } ;
