0b62835eeb951fd2bf967e7fdb4194649bdddd06
Laurent Bercot <ska-skaware@skarnet.org>
2017-08-14 18:35:05 +0000

    Revert initializer optimization (buggy when first realloc'ing!)

--- src/libdatastruct/gensetdyn_init.c
+++ src/libdatastruct/gensetdyn_init.c
@@ -1,13 +1,13 @@
 /* ISC license. */
 
-#include <stdint.h>
+#include <skalibs/stralloc.h>
 #include <skalibs/genalloc.h>
 #include <skalibs/gensetdyn.h>
 
 void gensetdyn_init (gensetdyn *g, uint32_t esize, uint32_t base, uint32_t fracnum, uint32_t fracden)
 {
-  g->storage.len = 0 ;
-  genalloc_setlen(uint32_t, &g->freelist, 0) ;
+  g->storage = stralloc_zero ;
+  g->freelist = genalloc_zero ;
   g->esize = esize ;
   g->base = base ;
   g->fracnum = fracnum ;
