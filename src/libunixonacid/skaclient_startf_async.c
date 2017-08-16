2e7eb0298199b542a1288272013ff259b77450a5
Laurent Bercot <ska-skaware@skarnet.org>
2017-08-16 23:17:45 +0000

    Make skaclient's fd coe

--- src/libunixonacid/skaclient_startf_async.c
+++ src/libunixonacid/skaclient_startf_async.c
@@ -32,7 +32,9 @@ int skaclient_startf_async (
   int fd ;
   pid_t pid = child_spawn1_socket(prog, argv, envp, &fd) ;
   if (!pid) return 0 ;
-  if (ndelay_on(fd) < 0 || !skaclient_init(a, fd, bufss, bufsn, auxbufss, auxbufsn, bufas, bufan, auxbufas, auxbufan, q, qlen, before, beforelen))
+  if (ndelay_on(fd) < 0
+   || coe(fd) < 0
+   || !skaclient_init(a, fd, bufss, bufsn, auxbufss, auxbufsn, bufas, bufan, auxbufas, auxbufan, q, qlen, before, beforelen))
   {
     fd_close(fd) ;
     if (options & SKACLIENT_OPTION_WAITPID)
