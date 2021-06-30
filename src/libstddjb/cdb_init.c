6c7a5b92bdfe68e847d371cc41f6655df3ef1ba5
Laurent Bercot <ska-skaware@skarnet.org>
2021-06-30 11:25:07 +0000

    bugfix: offset in cdb mmap
    An offset of -1 worked because most OSes treat it as 0,
    but DS9k would fail on it.
    
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/libstddjb/cdb_init.c
+++ src/libstddjb/cdb_init.c
@@ -15,7 +15,7 @@ int cdb_init (struct cdb *c, int fd)
   char *map ;
   if (fstat(fd, &st) < 0) return -1 ;
   if (st.st_size > UINT32_MAX) return (errno = EOVERFLOW, -1) ;
-  map = mmap(0, st.st_size, PROT_READ, MAP_SHARED, fd, -1) ;
+  map = mmap(0, st.st_size, PROT_READ, MAP_SHARED, fd, 0) ;
   if (map == MAP_FAILED) return -1 ;
   c->map = map ;
   c->size = st.st_size ;
--- src/libstddjb/cdb_mapfile.c
+++ src/libstddjb/cdb_mapfile.c
@@ -5,9 +5,9 @@
 
 int cdb_mapfile (struct cdb *c, char const *file)
 {
-  int fd = open_readb(file) ;
+  int fd = openc_read(file) ;
   if (fd < 0) return 0 ;
-  if (!cdb_init_map(c, fd, 1))
+  if (cdb_init(c, fd) == -1)
   {
     fd_close(fd) ;
     return 0 ;
