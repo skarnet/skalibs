122f9363682e5de8ce4056c4c05c1eaf8935cf19
Laurent Bercot <ska-skaware@skarnet.org>
2021-07-21 11:17:53 +0000

    Save errno when freeing a cdb
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/libstddjb/cdb_free.c
+++ src/libstddjb/cdb_free.c
@@ -1,10 +1,17 @@
 /* ISC license. */
 
 #include <sys/mman.h>
+#include <errno.h>
+
 #include <skalibs/cdb.h>
 
 extern void cdb_free (struct cdb *c)
 {
-  if (c->map) munmap(c->map, c->size) ;
+  if (c->map)
+  {
+    int e = errno ;
+    munmap(c->map, c->size) ;
+    errno = e ;
+  }
   *c = cdb_zero ;
 }
