686ca403f362ed0d92d94bec3f70984d5a012ca6
Laurent Bercot <ska-skaware@skarnet.org>
2021-09-02 15:46:12 +0000

    Don't forget textclient_commandv
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/libunixonacid/textclient_commandv.c
+++ src/libunixonacid/textclient_commandv.c
@@ -11,6 +11,6 @@ int textclient_commandv (textclient *a, struct iovec const *v, unsigned int n, t
   struct iovec ans ;
   if (!textclient_exchangev(a, v, n, &ans, deadline, stamp)) return 0 ;
   if (ans.iov_len != 1) return (errno = EPROTO, 0) ;
-  if (*(char *)ans.iov_base) return (errno = *(char *)ans.iov_base, 0) ;
+  if (*(unsigned char *)ans.iov_base) return (errno = *(unsigned char *)ans.iov_base, 0) ;
   return 1 ;
 }
