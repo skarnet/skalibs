8c4e1cb0b1ee824571d28f0f4e9e5250b18cc174
Laurent Bercot <ska-skaware@skarnet.org>
2021-09-02 14:29:42 +0000

    Infinite recursion my ass cheeks
     Sometimes clang warnings have a point.
    
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/libstddjb/uint640_scan_base_max.c
+++ src/libstddjb/uint640_scan_base_max.c
@@ -7,7 +7,7 @@
 
 size_t uint640_scan_base_max (char const *s, uint64_t *u, uint8_t base, uint64_t max)
 {
-  size_t pos = uint640_scan_base_max(s, u, base, max) ;
+  size_t pos = uint64_scan_base_max(s, u, base, max) ;
   if (!pos) return (errno = EINVAL, 0) ;
   if (!s[pos]) return pos ;
   errno = fmtscan_num(s[pos], base) < base ? ERANGE : EINVAL ;
