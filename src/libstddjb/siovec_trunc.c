ee2aeefa170b9806e719723f94ba933c90a9ae8b
Laurent Bercot <ska-skaware@skarnet.org>
2023-01-04 16:32:21 +0000

    Add siovec doc, fix siovec_trunc, credit Ermine
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/libstddjb/siovec_trunc.c
+++ src/libstddjb/siovec_trunc.c
@@ -6,13 +6,13 @@
 unsigned int siovec_trunc (struct iovec *v, unsigned int n, size_t len)
 {
   size_t w = siovec_len(v, n) ;
-  unsigned int i = n ;
   if (w < len) return n ;
   len = w - len ;
-  while (len && i--)
+  while (n && len)
   {
-    w = len > v[i].iov_len ? v[i].iov_len : len ;
-    v[i].iov_len -= w ; len -= w ;
+    w = len > v[n-1].iov_len ? v[n-1].iov_len : len ;
+    v[n-1].iov_len -= w ; len -= w ;
+    if (!v[n-1].iov_len) n-- ;
   }
-  return i ;
+  return n ;
 }
