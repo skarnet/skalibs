eb8bee59fdea19d6b34509c7c2ce6dbd459681a5
Laurent Bercot <ska-skaware@skarnet.org>
2023-02-12 02:38:09 +0000

    Tables should be const
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/libunixonacid/unixmessage_bits_closeall.c
+++ src/libunixonacid/unixmessage_bits_closeall.c
@@ -3,5 +3,5 @@
 #include <skalibs/bitarray.h>
 #include <skalibs/unixmessage.h>
 
-static unsigned char unixmessage_bits_closeall_[bitarray_div8(UNIXMESSAGE_MAXFDS)] = "\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377" ;
+static unsigned char const unixmessage_bits_closeall_[bitarray_div8(UNIXMESSAGE_MAXFDS)] = "\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377" ;
 unsigned char const *const unixmessage_bits_closeall = unixmessage_bits_closeall_ ;
