9964840c291e46a0e001d930e2dd2fb0e1b16967
Laurent Bercot <ska-skaware@skarnet.org>
2023-07-06 14:54:18 +0000

    More jjk fixes
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/libunixonacid/open_appendatb.c
+++ src/libunixonacid/open_appendatb.c
@@ -6,7 +6,11 @@
 int open_appendatb (int dirfd, char const *name)
 {
   int fd = open_appendat(dirfd, name) ;
-  if (fd < 0) return -1 ;
-  if (ndelay_off(fd) < 0) return -1 ;
+  if (fd == -1) return -1 ;
+  if (ndelay_off(fd) == -1)
+  {
+    fd_close(fd) ;
+    return -1 ;
+  }
   return fd ;
 }
--- src/libunixonacid/openc_writeatb.c
+++ src/libunixonacid/openc_writeatb.c
@@ -6,7 +6,11 @@
 int openc_writeatb (int dirfd, char const *name)
 {
   int fd = openc_writeat(dirfd, name) ;
-  if (fd < 0) return -1 ;
-  if (ndelay_off(fd) < 0) return -1 ;
+  if (fd == -1) return -1 ;
+  if (ndelay_off(fd) == -1)
+  {
+    fd_close(fd) ;
+    return -1 ;
+  }
   return fd ;
 }
