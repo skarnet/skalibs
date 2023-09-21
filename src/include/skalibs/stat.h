3b95fe65d951745988f072bca328ebe06a596e3b
Laurent Bercot <ska-skaware@skarnet.org>
2023-09-21 10:42:38 +0000

    Fix skalibs/stat.h comment
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/include/skalibs/stat.h
+++ src/include/skalibs/stat.h
@@ -4,9 +4,10 @@
 #define SKALIBS_STAT_H
 
  /*
-    This header must always be paired with skalibs/nonposix.h
+    This header must always be paired with skalibs/bsdsnowflake.h
     (which must always come first).
-    If you #include <sys/stat.h> before this, it will not work.
+    If you #include <sys/stat.h> before bsdsnowflake, the
+    workaround will not work.
  */
 
 #include <skalibs/sysdeps.h>
