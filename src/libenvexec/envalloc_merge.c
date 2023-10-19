83ff0ca30e725c25b8b7e6d78ff17246caf29cf8
Josiah Frentsos via skaware <skaware@list.skarnet.org>
2023-10-19 13:01:43 -0400

    Fix envalloc_merge
    Currently, envalloc_merge incorrectly passes the total number of
    environment entries to env_mergen as the number of modifs.
    
    Signed-off-by: Josiah Frentsos <jfrent@tilde.team>

--- src/libenvexec/envalloc_merge.c
+++ src/libenvexec/envalloc_merge.c
@@ -7,9 +7,10 @@
 
 int envalloc_merge (genalloc *v, char const *const *envp, size_t envlen, char const *modifs, size_t modiflen)
 {
-  size_t n = envlen + 1 + byte_count(modifs, modiflen, '\0') ;
+  size_t modifn = byte_count(modifs, modiflen, '\0') ;
+  size_t n = envlen + 1 + modifn ;
   if (!genalloc_readyplus(char const *, v, n)) return 0 ;
-  n = env_mergen(genalloc_s(char const *, v) + genalloc_len(char const *, v), n, envp, envlen, modifs, modiflen, n) ;
+  n = env_mergen(genalloc_s(char const *, v) + genalloc_len(char const *, v), n, envp, envlen, modifs, modiflen, modifn) ;
   genalloc_setlen(char const *, v, genalloc_len(char const *, v) + n) ;
   return 1 ;
 }
