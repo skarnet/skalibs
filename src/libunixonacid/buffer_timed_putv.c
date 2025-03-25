56ab53289c60c6fbb06f8d548d353535d5ed1140
Laurent Bercot <ska-skaware@skarnet.org>
2025-03-25 06:48:24 +0000

    Don't autoflush buffer_timed_putv; fix IFS bug in configure
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/libunixonacid/buffer_timed_putv.c
+++ src/libunixonacid/buffer_timed_putv.c
@@ -9,14 +9,15 @@
 size_t buffer_timed_putv (buffer *b, struct iovec const *v, unsigned int vlen, tain const *deadline, tain *stamp)
 {
   if (!vlen) return 0 ;
-  size_t tot = siovec_len(v, vlen), w = 0 ;
+  size_t tot = siovec_len(v, vlen) ;
+  size_t w = 0 ;
   struct iovec vv[vlen] ;
   for (unsigned int i = 0 ; i < vlen ; i++) vv[i] = v[i] ;
-  while (w < tot)
+  for (;;)
   {
     size_t r = buffer_putvnoflush(b, vv, vlen) ;
     w += r ; siovec_seek(vv, vlen, r) ;
-    if (!buffer_timed_flush(b, deadline, stamp)) break ;
+    if (w >= tot || !buffer_timed_flush(b, deadline, stamp)) break ;
   }
   return w ;
 }
