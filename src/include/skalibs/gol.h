b708eef95c98ba9215f5eef2c2911e4f87ebc7a5
Laurent Bercot <ska-skaware@skarnet.org>
2025-05-18 13:21:27 +0000

    Reorder gol struct members
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/include/skalibs/gol.h
+++ src/include/skalibs/gol.h
@@ -12,18 +12,18 @@
 typedef struct gol_bool_s gol_bool, *gol_bool_ref ;
 struct gol_bool_s
 {
+  uint64_t mask ;
   char const *lo ;
   uint8_t so : 7 ;
   uint8_t set : 1 ;
-  uint64_t mask ;
 } ;
 
 typedef struct gol_arg_s gol_arg, *gol_arg_ref ;
 struct gol_arg_s
 {
   char const *lo ;
-  uint8_t so : 7 ;
   unsigned int i ;
+  uint8_t so : 7 ;
 } ;
 
 extern int gol (char const *const *, unsigned int, gol_bool const *, unsigned int, gol_arg const *a, unsigned int, uint64_t *, char const **, int *) ;
