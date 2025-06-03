1cd52389792cb6ca9919112110e22565d5f46d6a
Laurent Bercot <ska-skaware@skarnet.org>
2025-06-03 20:34:16 +0000

    prog_pid_fill bugfix
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/libstddjb/prog_pid_fill.c
+++ src/libstddjb/prog_pid_fill.c
@@ -10,6 +10,6 @@ void prog_pid_fill (char *s, char const *name, size_t len)
 {
   memcpy(s, name, len) ; s += len ;
   memcpy(s, ": pid ", 6) ; s += 6 ;
-  s += pid_fmt(s, pid_fmt(s, getpid())) ;
+  s += pid_fmt(s, getpid()) ;
   *s++ = 0 ;
 }
