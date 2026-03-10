f9c256aec78bdb594c3d3719373d67712851f5b6
Laurent Bercot <ska-skaware@skarnet.org>
2026-03-10 17:44:37 +0000

    ZI stralloc/genalloc

--- src/libstddjb/stralloc_ready_tuned.c
+++ src/libstddjb/stralloc_ready_tuned.c
@@ -2,6 +2,7 @@
 
 #include <string.h>
 #include <errno.h>
+
 #include <skalibs/alloc.h>
 #include <skalibs/stralloc.h>
 
@@ -16,10 +17,12 @@ int stralloc_ready_tuned (stralloc *sa, size_t n, size_t base, size_t a, size_t 
     sa->s = alloc(t) ;
     if (!sa->s) return 0 ;
     sa->a = t ;
+    memset(sa->s, 0, t) ;
   }
   else if (n > sa->a)
   {
     if (!alloc_re(&sa->s, sa->a, t)) return 0 ;
+    memset(sa->s + sa->a, 0, t - sa->a) ;
     sa->a = t ;
   }
   return 1 ;
